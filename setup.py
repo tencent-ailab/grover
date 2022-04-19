2b103f555bfc3ec4f7de48438158a627b75410cd
atreyamaj <atreyamaj@gmail.com>
2022-04-19 21:25:02 +0530

    uptill command 3

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='grover',
+    version='1.0.0',
+    description='Installable Grover package from Tencent AI but modified.',
+    author='Atreya Majumdar',
+    url='https://github.com/atreyamaj/grover',
+    packages=find_packages()
+)
